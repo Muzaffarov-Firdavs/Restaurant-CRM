55348b2e246edfcaf7c31cca11c1d71f225baf80
Muzaffarov03 <firdavsmuzaffarov03@gmail.com>
2023-04-16 20:02:57 +0500

    finished password Hasher  #2

---
+++ Restaurant.Service/Helpers/PasswordHasher.cs
@@ -0,0 +1,19 @@
+﻿namespace Restaurant.Service.Helpers
+{
+    public class PasswordHasher
+    {
+        public static (string passwordHash, string salt) Hash(string password)
+        {
+            string salt = Guid.NewGuid().ToString();
+            string strongPassword = salt + password;
+            string passwordHash = BCrypt.Net.BCrypt.HashPassword(strongPassword);
+            return (passwordHash, salt);
+        }
+
+        public static bool Verify(string password, string salt, string passwordHash)
+        {
+            string strongPassword = salt + password;
+            return BCrypt.Net.BCrypt.Verify(strongPassword, passwordHash);
+        }
+    }
+}
