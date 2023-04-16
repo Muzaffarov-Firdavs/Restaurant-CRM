d1b2027e9b6541a6eb6632e2b2be4c8585640f23
Muzaffarov03 <firdavsmuzaffarov03@gmail.com>
2023-04-16 18:19:09 +0500

    finished soldFood service

--- Restaurant.Service/Interfaces/ISoldFoodService.cs
+++ Restaurant.Service/Interfaces/ISoldFoodService.cs
@@ -1,7 +1,16 @@
-﻿namespace Restaurant.Service.Interfaces
+﻿using Restaurant.Domain.Entities;
+using Restaurant.Service.DTOs.SoldFood;
+using System.Linq.Expressions;
+
+namespace Restaurant.Service.Interfaces
 {
     public interface ISoldFoodService
     {
-
+        Task<SoldFoodForResultDto> CreateAsync(SoldFoodForCreationDto dto);
+        Task<SoldFoodForResultDto> ChangeAsync(SoldFoodForUpdateDto dto);
+        Task<SoldFoodForResultDto> RetriewByIdAsync(long id);
+        Task<IEnumerable<SoldFoodForResultDto>> RetriewAllAsync(
+            Expression<Func<SoldFood, bool>> expression = null, string search = null);
+        Task<bool> RemoveAsync(long id);
     }
 }
--- Restaurant.Service/Services/SoldFoodService.cs
+++ Restaurant.Service/Services/SoldFoodService.cs
@@ -1,6 +1,80 @@
-﻿namespace Restaurant.Service.Services
+﻿using AutoMapper;
+using Restaurant.Data.IRepositories;
+using Restaurant.Domain.Entities;
+using Restaurant.Service.DTOs.SoldFood;
+using Restaurant.Service.Exceptions;
+using Restaurant.Service.Interfaces;
+using System.Linq.Expressions;
+
+namespace Restaurant.Service.Services
 {
-    public class SoldFoodService
+    public class SoldFoodService : ISoldFoodService
     {
+        private readonly IRepository<SoldFood> soldFoodRepository;
+        private readonly IMapper mapper;
+
+        public SoldFoodService(IRepository<SoldFood> soldFoodRepository, IMapper mapper)
+        {
+            this.soldFoodRepository = soldFoodRepository;
+            this.mapper = mapper;
+        }
+
+
+        public async Task<SoldFoodForResultDto> ChangeAsync(SoldFoodForUpdateDto dto)
+        {
+            var updatingSoldFood = await soldFoodRepository.SelectAsync(u => u.Id.Equals(dto.Id));
+            if (updatingSoldFood is null)
+                throw new CustomException(404, "SoldFood not found");
+
+            this.mapper.Map(dto, updatingSoldFood);
+            updatingSoldFood.UpdatedAt = DateTime.UtcNow;
+            await this.soldFoodRepository.SaveChangesAsync();
+            return mapper.Map<SoldFoodForResultDto>(updatingSoldFood);
+        }
+
+        public async Task<SoldFoodForResultDto> CreateAsync(SoldFoodForCreationDto dto)
+        {
+            var soldFood = await this.soldFoodRepository.SelectAsync(u => u.Name.ToLower() == dto.Name.ToLower());
+            if (soldFood is not null)
+                throw new CustomException(403, "SoldFood already exsists with email");
+
+            SoldFood mappedFood = mapper.Map<SoldFood>(dto);
+            var result = await this.soldFoodRepository.InsertAsync(mappedFood);
+            await this.soldFoodRepository.SaveChangesAsync();
+            return this.mapper.Map<SoldFoodForResultDto>(result);
+        }
+
+        public async Task<bool> RemoveAsync(long id)
+        {
+            var soldfood = await this.soldFoodRepository.SelectAsync(u => u.Id.Equals(id));
+            if (soldfood is null)
+                throw new CustomException(404, "SoldFood not found");
+
+            await this.soldFoodRepository.DeleteAsync(soldfood);
+            await this.soldFoodRepository.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<IEnumerable<SoldFoodForResultDto>> RetriewAllAsync(
+                    Expression<Func<SoldFood, bool>> expression = null, string search = null)
+        {
+            var soldFoods = soldFoodRepository.SelectAll(expression, isTracking: false);
+
+            var result = mapper.Map<IEnumerable<SoldFoodForResultDto>>(soldFoods);
+            if (string.IsNullOrEmpty(search))
+            {
+                return result.Where(
+                    u => u.Name.ToLower().Contains(search.ToLower())).ToList();
+            }
+            return result;
+        }
+
+        public async Task<SoldFoodForResultDto> RetriewByIdAsync(long id)
+        {
+            var soldFood = await soldFoodRepository.SelectAsync(u => u.Id.Equals(id));
+            if (soldFood is null)
+                throw new CustomException(404, "SoldFood not found");
+            return mapper.Map<SoldFoodForResultDto>(soldFood);
+        }
     }
 }
