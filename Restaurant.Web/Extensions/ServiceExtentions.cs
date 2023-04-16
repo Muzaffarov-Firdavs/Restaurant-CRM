22b75e2f7f53eb063fcda84ec36fbe91fc8b8a71
Muzaffarov03 <firdavsmuzaffarov03@gmail.com>
2023-04-16 22:03:40 +0500

    added service extentions to add DI conteiner

---
+++ Restaurant.Web/Extensions/ServiceExtentions.cs
@@ -0,0 +1,21 @@
+﻿using Restaurant.Data.IRepositories;
+using Restaurant.Data.Repositories;
+using Restaurant.Service.Interfaces;
+using Restaurant.Service.Services;
+
+namespace Restaurant.Web.Extensions
+{
+    public static class ServiceExtentions
+    {
+        public static void AddCustomServices(this IServiceCollection services)
+        {
+            // services
+            services.AddScoped<IUserService, UserService>();
+            services.AddScoped<ISoldFoodService, SoldFoodService>();
+            services.AddScoped<IFoodService, FoodService>();
+
+            // repository
+            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+        }
+    }
+}
