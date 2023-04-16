59046f9113b3b454c40dc634ef1a5dd28e531ef1
Muzaffarov03 <firdavsmuzaffarov03@gmail.com>
2023-04-16 16:45:58 +0500

    changed IUserService

--- Restaurant.Service/Interfaces/IUserService.cs
+++ Restaurant.Service/Interfaces/IUserService.cs
@@ -7,10 +7,10 @@ namespace Restaurant.Service.Interfaces
     public interface IUserService
     {
         Task<UserForResultDto> CreateAsync(UserForCreationDto dto);
-        Task<UserForResultDto> ChangeAsync(UserForCreationDto dto);
+        Task<UserForResultDto> ChangeAsync(UserForUpdateDto dto);
         Task<UserForResultDto> ChangePasswordAsync(UserForChangePassword dto);
         Task<UserForResultDto> RetriewByIdAsync(long id);
-        Task<IEnumerable<UserForResultDto>> GetAllAsync(
+        Task<IEnumerable<UserForResultDto>> RetriewAllAsync(
             Expression<Func<User, bool>> expression = null, string search = null);
         Task<bool> RemoveAsync(long id);
 
