f4a8ad95e2fd723001ab7c61071b0f575861d809
Muzaffarov03 <firdavsmuzaffarov03@gmail.com>
2023-04-16 18:56:49 +0500

    writen try to email verification

---
+++ Restaurant.Service/Helpers/EmailVerification.cs
@@ -0,0 +1,53 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.Extensions.Configuration;
+using MimeKit;
+using MimeKit.Text;
+using Restaurant.Service.DTOs.Users;
+using StackExchange.Redis;
+
+namespace Restaurant.Service.Helpers
+{
+    public class EmailVerification
+    {
+        private readonly IConfiguration configuration;
+
+        public EmailVerification(IConfiguration configuration)
+        {
+            this.configuration = configuration.GetSection("Email");
+        }
+
+        public async Task<string> SendAsync(UserForResultDto user)
+        {
+            try
+            {
+                Random random = new Random();
+                int verificationCode = random.Next(123456, 999999);
+
+                ConnectionMultiplexer redisConnect = ConnectionMultiplexer.Connect("localhost");
+                IDatabase db = redisConnect.GetDatabase();
+                db.StringSet("code", verificationCode.ToString());
+                var result = db.StringGet("code");
+
+                var email = new MimeMessage();
+                email.From.Add(MailboxAddress.Parse(this.configuration["EmailAddress"]));
+                email.To.Add(MailboxAddress.Parse(user.Email));
+                email.Subject = "Email verification for Admin!";
+                email.Body = new TextPart(TextFormat.Html) { Text = result.ToString() };
+
+                var sendMessage = new SmtpClient();
+                await sendMessage.ConnectAsync(this.configuration["Host"], 587, SecureSocketOptions.StartTls);
+                await sendMessage.AuthenticateAsync(this.configuration["EmailAddress"], this.configuration["Password"]);
+                await sendMessage.SendAsync(email);
+                await sendMessage.DisconnectAsync(true);
+
+                return result.ToString();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
