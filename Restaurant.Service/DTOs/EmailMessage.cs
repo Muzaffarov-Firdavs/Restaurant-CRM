5e52374da8439825ef86cca45bc6ae6b7b4bf0f7
Muzaffarov03 <firdavsmuzaffarov03@gmail.com>
2023-04-16 18:42:38 +0500

    create ne wEmail message dto

---
+++ Restaurant.Service/DTOs/EmailMessage.cs
@@ -0,0 +1,9 @@
+﻿namespace Restaurant.Service.DTOs
+{
+    public class EmailMessage
+    {
+        public string To { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+    }
+}
